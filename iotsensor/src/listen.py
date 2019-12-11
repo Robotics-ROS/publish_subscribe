b00189104d1e4e365dbe39cd6cdabaf956e48343
mridul-eecs <mridul.eecs@gmail.com>
2019-12-11 14:49:41 +0530

    Added client and server service implementation in iotsensor package

--- iotsensor/src/listen.py
+++ iotsensor/src/listen.py
@@ -2,6 +2,22 @@
 
 import rospy
 from iotsensor.msg import temp_humidity
+from iotsensor.srv import math
+import time
+
+def servicetest(x, y):
+    rospy.wait_for_service('mathSum')
+    try:
+        print('found service')
+        add= rospy.ServiceProxy('mathSum', math)
+        resp= add(x, y)
+        print 'got response', resp
+        return resp.sum
+        
+    except:
+        print('service call failed')
+        pass
+
 
 def callbacklistener(data):
     # rospy.loginfo(rospy.get_caller_id() + "Heard\n" + data)
@@ -12,5 +28,10 @@ def callbacklistener(data):
 if __name__ == "__main__":
     rospy.init_node('iotsensor_listener', anonymous= True)
     rospy.Subscriber('tempHumidityTransmit', temp_humidity, callbacklistener)
-    print('Subscribed')
+    while True:
+        servicetest(1, 5)
+        time.sleep(1)
+
+    # rospy.Service
+    # print('Subscribed')
     rospy.spin()
